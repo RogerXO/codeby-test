fa415bc48998bc8ac4d6836a72a3ffec25f3aacd
RogerXavier <rogerxavier10@gmail.com>
2022-03-12 19:21:58 -0300

    discount added

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,6 +1,16 @@
 import styles from "./ProductCard.module.css";
 
 function ProductCard({ id, name, imgUrl, price, sellingPrice }) {
+  const maskedPrice = (price / 100).toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+  const maskedSellingPrice = (sellingPrice / 100).toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+  const discount = Math.round((price / sellingPrice - 1) * 100)
+
   return (
     <div key={id} className={styles.productCard}>
       <div className={styles.image}>
@@ -9,8 +19,11 @@ function ProductCard({ id, name, imgUrl, price, sellingPrice }) {
       <div className={styles.info}>
         <h3>{name}</h3>
         <div className={styles.price}>
-          <span> {(price/100).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span>
-          <p>{(sellingPrice/100).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+          <span> {maskedPrice}</span>
+          <div className={styles.discount}>
+            <p>{maskedSellingPrice}</p>
+            <p className={styles.off}> -{discount}% de desconto</p>
+          </div>
         </div>
       </div>
     </div>
